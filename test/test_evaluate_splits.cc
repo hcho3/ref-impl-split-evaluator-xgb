2ad09b87162d8e25099366792aaf81ea2de7433f
Hyunsu Cho <chohyu01@cs.washington.edu>
2021-08-05 18:29:21 -0700

    Test tie breaking

--- test/test_evaluate_splits.cc
+++ test/test_evaluate_splits.cc
@@ -412,3 +412,34 @@ TEST(EvaluateSplits, EvaluateSplitsWithFeatureSampling) {
   EXPECT_FLOAT_EQ(out_splits[1].right_sum.sum_grad, 0.5);
   EXPECT_FLOAT_EQ(out_splits[1].right_sum.sum_hess, 0.5);
 }
+
+// Features 0 and 1 have identical gain, the algorithm must select 0
+TEST(EvaluateSplits, EvaluateSingleSplitBreakTies) {
+  std::vector<SplitCandidate> out_splits(1);
+  GradientPair parent_sum{0.0, 1.0};
+  TrainingParam param{0.0f};
+
+  std::vector<bst_feature_t> feature_set{0, 1};
+  std::vector<uint32_t> feature_segments{0, 2, 4};
+  std::vector<float> feature_values{1.0, 2.0, 11.0, 12.0};
+  std::vector<float> feature_min_values{0.0, 10.0};
+  std::vector<GradientPair> feature_histogram{
+    {-0.5, 0.5}, {0.5, 0.5}, {-0.5, 0.5}, {0.5, 0.5}};
+  EvaluateSplitInputs input{
+    1,
+    parent_sum,
+    param,
+    ToSpan(feature_set),
+    {},
+    ToSpan(feature_segments),
+    ToSpan(feature_values),
+    ToSpan(feature_min_values),
+    ToSpan(feature_histogram)};
+
+  SplitEvaluator evaluator;
+  EvaluateSingleSplit(ToSpan(out_splits), evaluator, input);
+
+  SplitCandidate result = out_splits[0];
+  EXPECT_EQ(result.findex, 0);
+  EXPECT_EQ(result.fvalue, 1.0);
+}
